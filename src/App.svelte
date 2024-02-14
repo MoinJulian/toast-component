46e5237a2fa6492280a40b41dbc9d84d68b13ae9
Julian Hammer <jh@moinjulian.com>
2024-02-14 12:37:50 +0100

    Setup

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Menu from "./lib/components/Menu.svelte";
+  import Toast from "./lib/components/Toast.svelte";
+
+</script>
+
+<Toast></Toast>
+
+<main>
+  <Menu></Menu>
+</main>
+
+<style>
+  main {
+    max-width: 32rem;
+    margin-inline: auto;
+  }
+</style>
---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,35 @@
+<menu>
+    <button>Send Toast</button>
+</menu>
+
+<style>
+	menu {
+		margin-top: 2rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	button {
+		border: none;
+		background: none;
+		font-family: inherit;
+		font-size: inherit;
+		cursor: pointer;
+		background-color: steelblue;
+		color: white;
+		padding: 0.5rem 1rem;
+		border-radius: 0.25rem;
+	}
+
+	/* select,
+	option {
+		font-family: inherit;
+		font-size: inherit;
+	}
+
+	select {
+		padding: 0.25rem;
+	} */
+</style>
